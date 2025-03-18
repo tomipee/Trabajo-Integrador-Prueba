44fba4a181edff7e8f65bf3080fefe2eb5d0f9f8
tomipee <tomaspesce.estrada@gmail.com>
2025-03-18 16:16:11 -0300

    sexto commit

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ print ("Generador de claves!")
 
 chars = string.ascii_letters + string.digits + string.punctuation
 password = ""
-length = int(input("Ingrese la longitud de la contraseña aquí"))
+length = int(input("Ingrese la longitud de la contraseña aquí porfavor"))
 
 
 for i in range (length):
