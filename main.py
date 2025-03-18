a10aad693284be237e8a95bcafb993e28245ee10
tomipee <tomaspesce.estrada@gmail.com>
2025-03-18 15:31:36 -0300

    Segundo commit

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print ("hola")
+import string
+import random
+
+print ("Generador de claves!")
+
+chars = string.ascii_letters + string.digits + string.punctuation
+password = ""
+length = 10
+
+
+for i in range (length):
+     password = password + random.choice(chars)
+     
+print ("Contraseña generada: ", password)
